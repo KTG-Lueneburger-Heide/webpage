949ffba562d8fe59732988bcc18243de34f2d722
Denis Kramer <d.kramer@soton.ac.uk>
2024-07-17 09:30:18 +0200

    Logo component with custom styling and instant loading page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import splashes from '@/data/splashes.yaml'
 import Banner from "@/components/banner";
 
 import Config from "@/app/app.config"
+import { LogoMinimal } from '@/components/logo';
 
 export default function Home() {
 
@@ -11,7 +12,9 @@ export default function Home() {
     <>        
       <Banner className="landscape:parallax-landing-landscape portrait:parallax-landing-portrait animate-blend-in animation-delay-200 opacity-0">
         <div className='w-screen h-screen flex justify-center items-center flex-col'>
-          <img src={Config.logo} className='w-[30em]'></img>
+          <div className='w-[30em]'>
+            <LogoMinimal style={{fill: "white"}}/>
+          </div>
           <div className='relative'>
             <div className='h-[2px] w-0 bg-white  top-0 left-0 absolute animate-grow-width animation-delay-500'></div>
             <h2 className='font-serif text-4xl mx-7 mt-5 mb-4 opacity-0 animate-blend-scale animation-delay-500'>{Config.title}</h2>
