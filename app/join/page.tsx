ec2889238b594d5e25708e5de491163efaa25e40
Denis Kramer <d.kramer@soton.ac.uk>
2024-07-23 11:13:57 +0200

    Code cleaning

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -3,10 +3,6 @@ import TrialRequestForm from "@/components/visuals/trailrequest"
 
 export default function JoinPage() {
 
-  const submitTrailRequest = async (email : string) => {
-
-  }
-
   return (
     <div className="w-screen min-h-screen parallax-pink-splash-sm md:parallax-pink-splash-md xl:parallax-pink-splash-xl portrait:parallax-pink-splash-portrait-sm portrait:md:parallax-pink-splash-portrait-md portrait:xl:parallax-pink-splash-portrait-xl pt-20 lg:pt-32 pb-20 text-center flex flex-col justify-center">
       <div className=""><h2 className="title inline-block">Mach mit</h2></div>
